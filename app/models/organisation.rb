a4c21e7f851db688a2553b3fb95464784136716a
Stacey McKnight <mcknight.stm@gmail.com>
2021-07-31 18:10:15 -0500

    add validations for attributes

--- app/models/organisation.rb
+++ app/models/organisation.rb
@@ -1,3 +1,8 @@
 class Organisation < ApplicationRecord
   has_many :users
+
+  validates :name, presence: true
+  validates :hourly_rate, numericality: { greater_than: -1 }
+  validates :hourly_rate, format: { with: /\A\d+(?:\.\d{0,2})?\z/,
+   message: "must have two decimal points" }
 end
