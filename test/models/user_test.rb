1dd5182b6e1b2fda217bbbdef2312bdd6e7b380d
Stacey McKnight <mcknight.stm@gmail.com>
2021-07-31 17:04:19 -0500

    add unit tests for user validations

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,46 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.new(
+      name: "Sali Mander",
+      email: "sali@aquaco.com",
+      password: "123456"
+    )
+  end
+
+  test "user should be valid" do
+    assert @user.valid?, "user is not valid"
+  end
+
+  test "password should be present" do
+    @user.password = " "
+    assert_not @user.valid?, "password is empty string"
+  end
+
+  test "password must be at least 6 characters long" do
+    @user.password = "12345"
+    assert_not @user.valid?, "password is not at least 6 characters"
+  end
+
+  test "name must be present" do
+    @user.name = " "
+    assert_not @user.valid?, "name is empty string"
+  end
+
+  test "email must be present" do
+    @user.email = " "
+    assert_not @user.valid?, "email is empty string"
+  end
+
+  test "email must be unique" do
+    @user.email = "user@company.com"
+    new_user = User.new(@user.attributes)
+    assert_not new_user.valid?, "email must be unique"
+  end
+
+  test "email should be valid address" do
+    @user.email = "emailaddress"
+    assert_not @user.valid?, "email must be valid address"
+  end
 end
