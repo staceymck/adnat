cab6623db53ae9feb6460baa78e2309c9e068150
Stacey McKnight <mcknight.stm@gmail.com>
2021-07-31 17:19:40 -0500

    add validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,14 @@
 class User < ApplicationRecord
+  has_secure_password
+
   belongs_to :organisation, optional: true
+  has_many :shifts
+
+  validates :password, length: { minimum: 6 }
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :email, uniqueness: true
+  # Use gem Email Validator in strict mode to validate email addresses
+  validates :email, email: {mode: :strict, require_fqdn: true}
+
 end
