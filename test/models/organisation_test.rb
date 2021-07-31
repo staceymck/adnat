7e86c9e5fa5f6434744ff959e3fd1c3803de4ad4
Stacey McKnight <mcknight.stm@gmail.com>
2021-07-31 18:09:32 -0500

    add unit tests for organisation model

--- test/models/organisation_test.rb
+++ test/models/organisation_test.rb
@@ -1,7 +1,41 @@
 require 'test_helper'
 
 class OrganisationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @org = Organisation.new(
+      name: "Center for Ants",
+      hourly_rate: 21
+    )
+  end
+
+  test "organisation should be valid" do
+    assert @org.valid?, "organisation is not valid"
+  end
+
+  test "organisation name must be present" do
+    @org.name = " "
+    assert_not @org.valid?, "name is empty string"
+  end
+
+  #should this be nil or " " or check against both?
+  test "organisation hourly rate must be present" do
+    @org.hourly_rate = nil
+    assert_not @org.valid?, "hourly rate is not present"
+  end
+
+  #not sure if I need this test
+  test "organisation hourly rate must not be empty string" do
+    @org.hourly_rate = " "
+    assert_not @org.valid?, "hourly rate is not present"
+  end
+
+  test "organisation hourly rate must be a decimal" do
+    @org.hourly_rate = "1D"
+    assert_not @org.valid?, "hourly rate is not a number"
+  end 
+
+  test "organisation hourly rate must not be negative" do
+    @org.hourly_rate = -1
+    assert_not @org.valid?, "hourly rate can't be negative"
+  end
 end
