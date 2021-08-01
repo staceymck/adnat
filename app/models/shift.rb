67d490801f9ccfae67cc63c0dd50f0c28100b224
Stacey McKnight <mcknight.stm@gmail.com>
2021-08-01 09:48:04 -0500

    add validations for shift attributes

--- app/models/shift.rb
+++ app/models/shift.rb
@@ -1,3 +1,9 @@
 class Shift < ApplicationRecord
   belongs_to :user
+
+  validates :break_length, numericality: { only_integer: true }
+
+  #validations available from validates_timeliness gem
+  validates_datetime :start, :finish
+  validates_date :start, on_or_before: :today
 end
