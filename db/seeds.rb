8ececcc94b333975944eb754d362122a900a7387
Stacey McKnight <mcknight.stm@gmail.com>
2021-08-01 10:35:01 -0500

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,50 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# This seed data can be loaded with the rails db:seed command (or created alongside the database with db:setup).
+
+# Create one user with no associated org
+User.create!(
+  name: "Joi Jones",
+  email: "joi@company.com",
+  password: "123456"
+)
+
+# Create orgs
+3.times do 
+  org = Organisation.create!(
+    name: Faker::Company.name,
+    hourly_rate: Faker::Number.decimal(l_digits: 2)
+  )
+end
+
+# Create employees for each organisation
+Organisation.all.each do |org|
+  3.times do
+    org.users.create!(
+      name: Faker::Name.name,
+      email: Faker::Internet.email,
+      password: Faker::Internet.password(min_length: 6)
+    )
+  end
+end
+
+# Create shifts for each employee
+User.all.each do |user|
+  # Generate standard shifts
+  3.times do
+    start_hr = rand(0..12)
+    finish_hr = start_hr + rand(6..10)
+    day = rand(1..20)
+
+    user.shifts.create!(
+      start: DateTime.new(2021,7,day,start_hr),
+      finish: DateTime.new(2021,7,day,finish_hr),
+      break_length: [15, 30, 45, 60].sample
+    )
+  end
+
+  # Generate overnight shift
+  user.shifts.create!(
+    start: DateTime.new(2021,7,21,18,30),
+    finish: DateTime.new(2021,7,22,3),
+    break_length: 45
+  )
+end
