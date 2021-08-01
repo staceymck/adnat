4b47c80078e3a38969b9b6da4d0eb10618adf235
Stacey McKnight <mcknight.stm@gmail.com>
2021-08-01 15:13:06 -0500

    add user auth methods for controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,21 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user  # makes method available in views to assist with conditional display logic
+  before_action :require_login
+
+  private
+  def current_user
+    @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def require_login
+    redirect_to root_path if !logged_in?
+  end
+
+  def redirect_if_logged_in
+    redirect_to dashboard_path if logged_in?
+  end
 end
