ee36eb01fcddd0f8c1f4a46021189a5a66c4d5e6
Stacey McKnight <mcknight.stm@gmail.com>
2021-08-02 15:01:04 -0500

    add routes for working with shifts

--- config/routes.rb
+++ config/routes.rb
@@ -3,16 +3,19 @@ Rails.application.routes.draw do
 
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
-  
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
 
   get '/dashboard', to: 'dashboard#show'
 
-  resources :users, except: [:new, :create, :destroy, :index]
-  resources :organisations, except: [:index, :show, :new]
-  
   post '/join', to: 'memberships#create'
   delete '/leave', to: 'memberships#destroy'
+
+  resources :users, except: [:new, :create, :destroy, :index] 
+  resources :organisations, except: [:index, :show, :new] do
+    resources :shifts, only: [:index]
+  end
+
+  resources :shifts, except: [:index, :show]
 end
