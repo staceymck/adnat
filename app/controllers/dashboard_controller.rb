60a43f5457e8decca6344849326ba6869f0e747f
Stacey McKnight <mcknight.stm@gmail.com>
2021-08-02 11:38:11 -0500

    render different view based on membership status

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,13 @@
 class DashboardController < ApplicationController
   def show
     @user = current_user
+    
+    if @user.organisation
+      @organisation = @user.organisation
+      render 'member_show'
+    else
+      @organisations = Organisation.all
+      render 'non_member_show'
+    end
   end
 end
