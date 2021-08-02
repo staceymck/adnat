c8c9068109c2131a4f93798bfd5fc89e9046d957
Stacey McKnight <mcknight.stm@gmail.com>
2021-08-01 19:18:04 -0500

    allow users to update account details without password update/entry

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,12 @@ class User < ApplicationRecord
   belongs_to :organisation, optional: true
   has_many :shifts
 
-  validates :password, length: { minimum: 6 }
+  # allow_blank: true so length validation runs only if password is present
+  # Allows user to update their details (email, name) without having to update their password at same time
+  validates :password, length: { minimum: 6, allow_blank: true }
   validates :name, presence: true
   validates :email, presence: true
   validates :email, uniqueness: true
   # Use gem Email Validator in strict mode to validate email addresses
   validates :email, email: {mode: :strict, require_fqdn: true}
-
 end
