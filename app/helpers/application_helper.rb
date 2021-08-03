b4bdd269552db4cc6ec6d2f94e24da4adadf12a1
Stacey McKnight <mcknight.stm@gmail.com>
2021-08-03 16:36:44 -0500

    add generic way to display no-record message

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def no_table_records_msg(collection)
     (tag.tr tag.td('No records to display')) if collection.size == 0 
   end
+
+  def no_records_msg(collection)
+    'No records to display' if collection.size == 0
+  end
 end
