f378dbd0f0b5b6c32b5ab93497863ed3368f5bdc
Stacey McKnight <mcknight.stm@gmail.com>
2021-08-03 11:48:43 -0500

    add custom accessors to handle virtual attributes

--- app/models/shift.rb
+++ app/models/shift.rb
@@ -1,9 +1,31 @@
 class Shift < ApplicationRecord
+  attr_accessor :date, :start_time, :finish_time, :overnight
   belongs_to :user
-
+  
+  before_validation :set_start_datetime, :set_finish_time
   validates :break_length, numericality: { only_integer: true }
-
-  #validations available from validates_timeliness gem
-  validates_datetime :start, :finish
+  validates_datetime :start
+  validates_datetime :finish
   validates_date :start, on_or_before: :today
+
+  scope :newest, -> {order(start: :desc)}
+
+  def overnight
+   self[:start].day < self[:finish].day
+  end
+
+  private 
+  def set_start_datetime
+    self[:start] = date + " " + start_time
+  end
+
+  def set_finish_time
+    if finish_time < start_time
+      # Increase date by one for overnight shift
+      next_day = Date.parse(date) + 1
+      self[:finish] = next_day.to_s + " " + finish_time
+    else
+      self[:finish] = date + " " + finish_time
+    end
+  end
 end
