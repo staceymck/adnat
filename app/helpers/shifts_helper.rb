3a9706b71a5618810b7d010e0da28b01fd74eb0b
Stacey McKnight <mcknight.stm@gmail.com>
2021-08-03 11:51:53 -0500

    add helpers for displaying values in edit/new shift form partial

--- app/helpers/shifts_helper.rb
+++ app/helpers/shifts_helper.rb
@@ -8,12 +8,25 @@ module ShiftsHelper
     datetime_obj.strftime("%l:%M %p")
   end
 
+  def form_date(shift)
+    shift.try(:start).try(:to_date)
+  end
+
+  def form_start_time(shift)
+    shift.try(:start).try(:strftime, "%T.%L")
+  end
+
+  def form_finish_time(shift)
+    shift.try(:finish).try(:strftime, "%T.%L")
+  end
+
   def hours_worked(shift)
     break_hours = shift.break_length/60.to_f
     TimeDifference.between(shift.start, shift.finish).in_hours - break_hours
   end
 
   def shift_cost(shift, organisation)
-    hours_worked(shift) * organisation.hourly_rate
+    cost = hours_worked(shift) * organisation.hourly_rate
+    number_to_currency(cost)
   end
 end
